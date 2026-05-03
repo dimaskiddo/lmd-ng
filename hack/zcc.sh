a90aa22e8c93818d2516315383b6dbe1e8ea6205
Dimas Restu H <drh.dimasrestu@gmail.com>
2026-05-03 19:11:40 +0700

    feat: update zig c and cpp wrapper script to include hardened build flags

--- hack/zcc.sh
+++ hack/zcc.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 ZIG_TARGET=""
+ZIG_SYSROOT=""
 
-SYSROOT=""
-MACOS_SDK=${MACOS_SDK:-"./MacOS.sdk"}
+ZIG_MACOS_SDK=${ZIG_MACOS_SDK:-"./MacOS.sdk"}
 
 ZIG_CMD="zig cc"
+ZIG_BUILD_FLAGS="-D_FORTIFY_SOURCE=3 -O3 -fstack-protector-strong -fcf-protection=full -fPIC"
+ZIG_BUILD_LDFLAGS="-Wl,-s -Wl,-z,relro,-z,now"
 
 # Check the name of the command used to invoke this script
 if [ "$(basename "$0")" = "zcxx.sh" ]; then
@@ -14,16 +16,19 @@ fi
 
 # Map Go's OS and Arch to Zig's targets
 case "$GOOS-$GOARCH" in
+    "linux-386")     ZIG_TARGET="x86-linux-gnu" ;;
     "linux-amd64")   ZIG_TARGET="x86_64-linux-gnu" ;;
     "linux-arm64")   ZIG_TARGET="aarch64-linux-gnu" ;;
+    "windows-386")   ZIG_TARGET="x86-windows-gnu" ;;
     "windows-amd64") ZIG_TARGET="x86_64-windows-gnu" ;;
+    "windows-arm64") ZIG_TARGET="aarch64-windows-gnu" ;;
     "darwin-amd64")  
         ZIG_TARGET="x86_64-macos-gnu"
-        SYSROOT="-isysroot $MACOS_SDK" 
+        ZIG_SYSROOT="-isysroot $ZIG_MACOS_SDK" 
         ;;
     "darwin-arm64")  
         ZIG_TARGET="aarch64-macos-gnu"
-        SYSROOT="-isysroot $MACOS_SDK" 
+        ZIG_SYSROOT="-isysroot $ZIG_MACOS_SDK" 
         ;;
     *)
         echo "Unsupported target: $GOOS-$GOARCH"
@@ -32,4 +37,4 @@ case "$GOOS-$GOARCH" in
 esac
 
 # Execute the correct compiler dynamically
-exec $ZIG_CMD -target $ZIG_TARGET $SYSROOT "$@"
+exec $ZIG_CMD $ZIG_BUILD_FLAGS $ZIG_BUILD_LDFLAGS -target $ZIG_TARGET $ZIG_SYSROOT "$@"
